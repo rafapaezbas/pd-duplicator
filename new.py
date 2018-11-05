9b58f5a8609abb5f332b1d6efabe755d411bb844
Paez Bastida <XPAEZR@de.softlab.net>
2018-11-05 17:35:32 +0100

    added multiple times duplication

--- new.py
+++ new.py
@@ -1,14 +1,16 @@
 objects = 0
 offset = 50
 vars = {}
+duplication_times = 3
+file = 'C:/Users/xpaezr/Desktop/patch.pd'
+originalFile = []
 
 def isObj(tokens):
 	if len(tokens) > 1 and tokens[1] == 'floatatom' or tokens[1] == 'obj':
 		return True
 	else:
 		return False
-		
-
+	
 def isWire(tokens):
 	if len(tokens) > 1 and tokens[1] == 'connect':
 		return True
@@ -16,50 +18,55 @@ def isWire(tokens):
 		return False
 		
 #look for variables
-with open('C:/Users/xpaezr/Desktop/patch.pd','r+') as f:
+with open(file,'r') as f:
+	originalFile = f.readlines();
 	lines = f.readlines()
 	for line in lines:
 		tokens = line.split()
 		if len(tokens) > 5 and tokens[1] == 'text' and tokens[4] == 'VAR':
 			vars[tokens[5]] = []
-			
 			index = 6
 			while index < len(tokens):
 				vars[tokens[5]].append(tokens[index])
 				index = index + 1
-			
-			
-print(vars)
 
-#read lines form file and duplicate objects
-with open('C:/Users/xpaezr/Desktop/patch.pd','r+') as f:
-	lines = f.readlines()
-	#check if every line is an object
-	for line in lines:
-		if isObj(line.split()):
-			objects = objects + 1
-			duplicatedLine = line.split()
-			#change position value
-			#parse to int in order to increment
-			duplicatedLine[2] = int(duplicatedLine[2]) + offset
-			#parse to str in order to print
-			duplicatedLine[2] = str(duplicatedLine[2])
-			newLine = "\n" + " ".join(duplicatedLine) 
-			f.write(newLine)
-			
-#read lines form file and duplicate wires
-with open('C:/Users/xpaezr/Desktop/patch.pd','r+') as f:
-	lines = f.readlines()
-	#check if every line is an object
-	for line in lines:
-		if isWire(line.split()):
-			duplicatedLine = line.split()
-			#change connections value
-			#parse to int in order to increment
-			duplicatedLine[2] = int(duplicatedLine[2]) + objects
-			duplicatedLine[4] = int(duplicatedLine[4]) + objects
-			#parse to str in order to print
-			duplicatedLine[2] = str(duplicatedLine[2])
-			duplicatedLine[4] = str(duplicatedLine[4])
-			newLine = "\n" + " ".join(duplicatedLine) 
-			f.write(newLine)
+print(originalFile)
+
+#count objects
+for line in originalFile:
+			if isObj(line.split()):
+				objects = objects + 1
+		
+				
+index = 1
+while index < duplication_times:	
+	#read lines form file and duplicate objects
+	with open(file,'a+') as f:
+		for line in originalFile:
+			if isObj(line.split()):
+				duplicatedLine = line.split()
+				#change if object is one of the vars, var is a dictionary
+				if duplicatedLine[3] in vars:
+					duplicatedLine[3] = duplicatedLine[3] + " " + str(vars[duplicatedLine[3]][index - 1])
+				#change position value
+				#parse to int in order to increment
+				duplicatedLine[2] = int(duplicatedLine[2]) + (offset * index)
+				#parse to str in order to print
+				duplicatedLine[2] = str(duplicatedLine[2])
+				newLine = "\n" + " ".join(duplicatedLine) 
+				f.write(newLine)
+	#read lines form file and duplicate wires, it has to be done once the objcets has been counted
+	with open(file,'a+') as f:
+		for line in originalFile:
+			if isWire(line.split()):
+				duplicatedLine = line.split()
+				#change connections value
+				#parse to int in order to increment
+				duplicatedLine[2] = int(duplicatedLine[2]) + (objects * index)
+				duplicatedLine[4] = int(duplicatedLine[4]) + (objects * index)
+				#parse to str in order to print
+				duplicatedLine[2] = str(duplicatedLine[2])
+				duplicatedLine[4] = str(duplicatedLine[4])
+				newLine = "\n" + " ".join(duplicatedLine) 
+				f.write(newLine)
+	index = index + 1
